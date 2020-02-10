65056bb0d48d8e1e99adc69bc1a686e476333b31
JoniMujica <jonimujica3@hotmail.com>
2020-02-10 15:18:41 -0300

    Solucionamos problema con el mes

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function fecha (){
     let f = new Date();
     
-    let fecha = f.getDate() + '/' + f.getMonth() + '/' + f.getFullYear() + ' ' 
+    let fecha = f.getDate() + '/' + (f.getMonth()+ 1) + '/' + f.getFullYear() + ' ' 
     + f.getHours() + ':' + f.getMinutes() + ':' + f.getSeconds() + '.' + f.getMilliseconds();
     return fecha;
 }
